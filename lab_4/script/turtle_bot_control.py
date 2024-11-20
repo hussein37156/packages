f973d6a07c8bcac2fc223fc70a37461a7b27e329
Hussein Hamed <hussein37156@gmail.com>
2024-11-20 12:09:51 +0200

    lab_4

--- lab_4/script/turtle_bot_control.py
+++ lab_4/script/turtle_bot_control.py
@@ -2,19 +2,16 @@
 import rospy
 from geometry_msgs.msg import Twist
 def Move_Robot():
-    rospy.init_node('Move_Robot', anonymous=True)
+    rospy.init_node('turtle_bot_control', anonymous=False)
     vel_msg = Twist()
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
     rate = rospy.Rate(10)
-    # Define the robot parameters required for simulation: 
-    r = 0.1 #the wheel radius in meters
-    l= 0.2 #the wheel base distance in meters (width of robot)
-    # Define the wheel velocity:
-    Wr = 2 #Angular velocity of the right wheel
-    Wl = 2 #Angular velocity of the left wheel
-    #Calculate the linear and angular velocities
-    v = (r/2) * (Wr + Wl) #Linear Velocity
-    w = (r/l) * (Wr - Wl) #Angular Velocity
+    r = 0.1
+    l= 0.2 
+    Wr = 7 
+    Wl = 5 
+    v = (r/2) * (Wr + Wl) 
+    w = (r/l) * (Wr - Wl) 
     vel_msg.linear.x = v
     vel_msg.linear.y = 0.0
     vel_msg.linear.z = 0.0
